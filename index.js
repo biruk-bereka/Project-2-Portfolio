7b4ab457706d19022f43e67aab0187f9aba52f74
Brenda309 <brewihogora23@gmail.com>
2022-03-08 17:58:48 +0200

    Add the style to the menu list

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const menuIcon = document.querySelector('#menu');
 
 menuIcon.addEventListener('click', () => {
+    const menuList = document.getElementById('list-items');
     document.getElementById('menu').style.display = 'none';
     document.getElementById('logo').style.display = 'none';
     document.querySelector('.header').style.display = 'block';
     document.getElementById('menu-list').style.display = 'block';
     document.getElementById('mail-icon').style.display = 'none';
+    menuList.classList.add('list-item');
 });
