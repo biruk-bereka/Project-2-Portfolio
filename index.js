d129459cbd385b5ea65b4bbf382517da258675e8
biruk-bereka <birukbeletech1212@gmail.com>
2022-03-10 18:42:46 +0300

    fix linter error

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ menuIcon.addEventListener('click', () => {
   document.querySelector('.header').style.display = 'block';
   document.getElementById('menu-list').style.display = 'block';
   document.getElementById('mail-icon').style.display = 'none';
-
   menuList.classList.add('list-item');
   listItems.classList.add('menu-item-style');
 });
@@ -22,45 +21,36 @@ menuItem.addEventListener('click', () => {
   window.location.reload();
 });
 
-// Popup window 
+// Popup window
 
 const seeProjectButton = document.querySelectorAll('.button');
 const projectCard = document.querySelectorAll('.project-card');
-const windowSize = window.matchMedia("(min-width: 992px)"); 
+const windowSize = window.matchMedia('(min-width: 992px)');
 
-for(let i=0; i<seeProjectButton.length; i++){
-    seeProjectButton[i].addEventListener('click', () => {
-    displayProjectCard(i); 
-});
-}
-
-
-
-function displayProjectCard(i){
+function displayProjectCard(i) {
   let projectTitle = projectCard[i].children[0];
   const projectTechnologies = projectCard[1].children[3];
 
-  if(i===0) {
+  if (i === 0) {
     projectTitle = document.querySelector('#project-title-1');
   }
-   
+
   const project = [{
     closePopup: './images/popup-cancel.png',
     title: `${projectTitle.innerHTML}`,
     technologies: `${projectTechnologies.innerHTML}`,
     popupImage: './images/popup-image.png',
-    description:  `<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    description: `<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                       Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
                       when an unknown printer took a galley of type and scrambled it to make a 
                       type specimen book. It has survived not only five centuries, but also the leap 
                       into electronic typesetting, remaining essent</p>`,
 
   }];
-  
-    
-  if(windowSize.matches) {
-     project[0].popupImage = './images/desktop-popup-image.png';
-    project[0].title = '<h1>Keeping track of hundreds of components website</h1>'; 
+
+  if (windowSize.matches) {
+    project[0].popupImage = './images/desktop-popup-image.png';
+    project[0].title = '<h1>Keeping track of hundreds of components website</h1>';
   }
 
   const htmlMarkup = `
@@ -79,7 +69,7 @@ function displayProjectCard(i){
     <button class="button popupButton"> See Live<img class="icon-info" src="./Icons/Group.png"></button>
     <div>
   `;
- 
+
   const section = document.createElement('section');
   section.className = 'default';
   document.body.appendChild(section);
@@ -90,12 +80,14 @@ function displayProjectCard(i){
   function display() {
     section.classList.toggle('popup');
     window.location.reload();
-  };
-  
-  closeIcon.addEventListener('click', display) 
-    section.classList.toggle('popup');
-  
-  
+  }
+
+  closeIcon.addEventListener('click', display);
+  section.classList.toggle('popup');
 }
 
-
+for (let i = 0; i < seeProjectButton.length; i += 1) {
+  seeProjectButton[i].addEventListener('click', () => {
+    displayProjectCard(i);
+  });
+}
