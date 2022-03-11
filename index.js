46f56a77ca4b0186d54d6be4f97a002697075af2
biruk-bereka <birukbeletech1212@gmail.com>
2022-03-11 19:46:53 +0300

    add mobile version project cards dynamically

--- index.js
+++ index.js
@@ -29,24 +29,109 @@ const projectCardsContainer = document.createElement('div');
 projectCardsContainer.classList.add('project-cards-container');
 projectSection.append(projectCardsContainer);
 
-function displayProjectCards() {
-  const projectCards = [
-    {
-      name: 'Profesional Art Printing Data',
-      discription: "A daily selection of privately personalized reads; no account or sign-ups required. has been the industry's standard ",
-      technologies: ['html', 'Bootstrap', 'Ruby on rails'],
-      buttonName: 'See Project',
-      liveVersionLink: 'https://biruk-bereka.github.io/Project-2-Portfolio/',
-      sourceLink: 'https://github.com/biruk-bereka/Project-2-Portfolio',
-    },
-  ];
-  console.log(projectCards);
+const projectCards = [
+  {
+    name: 'Profesional Art Printing Data 1',
+    discription: "A daily selection of privately personalized reads; no account or sign-ups required. has been the industry's standard ",
+    technologies: ['html', 'Bootstrap', 'Ruby on rails'],
+    image: './images/background-image.png',
+    buttonName: 'See Project',
+    liveVersionLink: 'https://biruk-bereka.github.io/Project-2-Portfolio/',
+    sourceLink: 'https://github.com/biruk-bereka/Project-2-Portfolio',
+  },
 
+  {
+    name: 'Profesional Art Printing Data 2',
+    discription: "A daily selection of privately personalized reads; no account or sign-ups required. has been the industry's standard ",
+    technologies: ['html', 'Bootstrap', 'Ruby on rails'],
+    image: './images/background-image.png',
+    buttonName: 'See Project',
+    liveVersionLink: 'https://biruk-bereka.github.io/Project-2-Portfolio/',
+    sourceLink: 'https://github.com/biruk-bereka/Project-2-Portfolio',
+  },
+
+  {
+    name: 'Profesional Art Printing Data 3',
+    discription: "A daily selection of privately personalized reads; no account or sign-ups required. has been the industry's standard ",
+    technologies: ['html', 'Bootstrap', 'Ruby on rails'],
+    image: './images/background-image.png',
+    buttonName: 'See Project',
+    liveVersionLink: 'https://biruk-bereka.github.io/Project-2-Portfolio/',
+    sourceLink: 'https://github.com/biruk-bereka/Project-2-Portfolio',
+  },
+
+  {
+    name: 'Profesional Art Printing Data 4',
+    discription: "A daily selection of privately personalized reads; no account or sign-ups required. has been the industry's standard ",
+    technologies: ['html', 'Bootstrap', 'Ruby on rails'],
+    image: './images/background-image.png',
+    buttonName: 'See Project',
+    liveVersionLink: 'https://biruk-bereka.github.io/Project-2-Portfolio/',
+    sourceLink: 'https://github.com/biruk-bereka/Project-2-Portfolio',
+  },
+
+  {
+    name: 'Profesional Art Printing Data 5',
+    discription: "A daily selection of privately personalized reads; no account or sign-ups required. has been the industry's standard ",
+    technologies: ['html', 'Bootstrap', 'Ruby on rails'],
+    image: './images/background-image.png',
+    buttonName: 'See Project',
+    liveVersionLink: 'https://biruk-bereka.github.io/Project-2-Portfolio/',
+    sourceLink: 'https://github.com/biruk-bereka/Project-2-Portfolio',
+  },
+
+  {
+    name: 'Profesional Art Printing Data 6',
+    discription: "A daily selection of privately personalized reads; no account or sign-ups required. has been the industry's standard ",
+    technologies: ['html', 'Bootstrap', 'Ruby on rails'],
+    image: './images/background-image.png',
+    buttonName: 'See Project',
+    liveVersionLink: 'https://biruk-bereka.github.io/Project-2-Portfolio/',
+    sourceLink: 'https://github.com/biruk-bereka/Project-2-Portfolio',
+  },
+];
+
+// creating cards
+for (let i = 0; i < projectCards.length; i += 1) {
   const card = document.createElement('div');
-  const h2 = document.createElement('h2');
+  card.classList.add('project-card', 'card-1');
+
+  const projectName = document.createElement('h2');
+  projectName.append(projectCards[i].name);
+
+  projectName.className = 'project-title';
+  const projectDiscription = document.createElement('p');
+  projectDiscription.append(projectCards[i].discription);
+  projectDiscription.classList.add('project-discription');
+
+  const projectTechnologies = document.createElement('ul');
+  projectTechnologies.className = 'technologies';
+
+  for (let j = 0; j < projectCards[i].technologies.length; j += 1) {
+    const technologyList = document.createElement('li');
+    technologyList.append(projectCards[i].technologies[j]);
+    projectTechnologies.append(technologyList);
+  }
+
+  const seeProject = document.createElement('button');
+  seeProject.append(projectCards[i].buttonName);
+  seeProject.className = 'button';
+
+  card.append(projectName, projectDiscription, projectTechnologies, seeProject);
   projectCardsContainer.append(card);
-  card.append(h2);
 }
 
-displayProjectCards();
-// Popup window end
+// creating popup
+
+// function displayPopup(){
+//   projectCardsContainer.classList.toggle('popup');
+//   window.location.reload();
+// }
+
+//  const seeProjectButton = document.querySelectorAll('.button');
+
+//  seeProjectButton.addEventListener('click', ()=> {
+//    for(let i=0; i<seeProjectButton.length; i+=1){
+//      seeProjectButton[i]
+//    }
+//  })
